577b7561ee9854453c3a0474aa6052cd703226e7
tal0311 <tal.amit0311@gmail.com>
2024-01-28 18:03:23 +0200

    added comment event type

--- js/main.controller.js
+++ js/main.controller.js
@@ -1,25 +1,45 @@
+window.onInit = onInit;
 
-window.onInit= onInit 
-
-
-function onInit(){
-  addEventListeners()
+function onInit() {
+  addEventListeners();
+  setHeaderSrc();
 }
 
-function sendMsgToIframe(payload){
-  const iframe = document.querySelector('#nav_iframe')
-  iframe.contentWindow.postMessage({type:'display_user', payload:payload}, '*')
+function sendDataToIframe(selector, type, payload) {
+  const iframe = document.querySelector(selector);
+  iframe.contentWindow.postMessage({ type, payload }, "*");
 }
 
+function addEventListeners() {
+  window.addEventListener(
+    "message",
+    (event) => {
+      const { type, payload } = event.data;
+      if (type === "display_user") {
+        console.log("display_user", payload);
+        sendDataToIframe("#nav_iframe", "display_user", payload);
+      }
+      if (type === "display_post") {
+        console.log("display_post", payload);
+        sendDataToIframe("#aside_iframe", "display_post", payload);
+      }
+      if (type === "update-post") {
+        sendDataToIframe("#feed_iframe", "update_post", payload);
+      }
+    },
+    false
+  );
+}
 
+function setHeaderSrc() {
+  const elIframe = document.querySelector("#header_iframe");
+  const src =
+    window.location.hostname === "localhost" ||
+    window.location.hostname.startsWith("127.0.0.1")
+      ? "http://127.0.0.1:8080"
+      : "https://tal0311.github.io/MFE-header/";
 
-function addEventListeners(){
-  window.addEventListener('message', function(event) {
-    console.log('event', event.data);
-    const {type, payload} =event.data
-    if(type ==='display_user'){
-      sendMsgToIframe(payload)
-    }
-    
-  } , false);
+  console.log("src", src);
+
+  elIframe.src = src;
 }
