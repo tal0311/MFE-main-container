1c69a3d6492ca1a734b685e3c4770bbc8db4bf94
tal0311 <tal.amit0311@gmail.com>
2024-01-29 16:01:12 +0200

    bug-fix: cancel button activating submission event

--- js/main.controller.js
+++ js/main.controller.js
@@ -1,6 +1,8 @@
 window.onInit = onInit;
+window.closeModal = closeModal;
 
 function onInit() {
+openModalByParams()
   addEventListeners();
   setHeaderSrc();
 }
@@ -16,19 +18,63 @@ function addEventListeners() {
     (event) => {
       const { type, payload } = event.data;
       if (type === "display_user") {
-        console.log("display_user", payload);
         sendDataToIframe("#nav_iframe", "display_user", payload);
       }
       if (type === "display_post") {
         console.log("display_post", payload);
         sendDataToIframe("#aside_iframe", "display_post", payload);
       }
-      if (type === "update-post") {
+      if (type === "update_post") {
         sendDataToIframe("#feed_iframe", "update_post", payload);
+      }
+      if (type === "display_modal") {
+        openModal();
       }
     },
     false
   );
+}
+
+function openModal() {
+  const elModal = document.querySelector(".add-post-dialog");
+  elModal.showModal();
+  handleUrlParams("modal", true);
+
+}
+
+function openModalByParams() {
+  const url = new URL(window.location.href);
+  const modal = url.searchParams.get("modal");
+  if (modal) {
+    openModal();
+  }
+}
+
+
+function closeModal(ev) {
+  debugger
+  if (ev) {
+  console.log('ev', ev);
+    ev.preventDefault();
+    const formData = Object.fromEntries(new FormData(ev.target));
+    sendDataToIframe('#feed_iframe', 'add_post', formData)
+    ev.target.reset()
+  }
+  const elModal = document.querySelector(".add-post-dialog");
+  elModal.close();
+  handleUrlParams('modal', false)
+}
+
+
+function handleUrlParams(paramsName, paramValue){
+  const url = new URL(window.location.href);
+  if(paramValue && paramValue ){
+    url.searchParams.set(paramsName, paramValue);
+  }else{
+    url.searchParams.delete(paramsName);
+  }
+  window.history.pushState({}, "", url);
+
 }
 
 function setHeaderSrc() {
@@ -38,8 +84,6 @@ function setHeaderSrc() {
     window.location.hostname.startsWith("127.0.0.1")
       ? "http://127.0.0.1:8080"
       : "https://tal0311.github.io/MFE-header/";
-
-  console.log("src", src);
 
   elIframe.src = src;
 }
